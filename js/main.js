5472a65232a93da9801c2b7a62efb0c8e013261d
sarahholden <sarahbeth.holden@gmail.com>
2015-11-27 18:25:02 -0800

    before contact_form

--- js/main.js
+++ js/main.js
@@ -1,65 +1,42 @@
 $( function() {
-  $('#slider').cbpFWSlider();
-
 
   // Sticky nav
   var $nav = $('.nav-wrapper nav'),
-  		$navWrapper = $('.nav-wrapper'),
+  		$aboutSection = $('#about'),
   		$navItems = $nav.find('a'),
   		$navHeight = $nav.outerHeight() + 1,
-  		navOffset = $navWrapper.offset().top;
+  		aboutOffset = $aboutSection.offset().top;
     	
-	    // Anchors corresponding to menu items
-	    scrollItems = $navItems.map(function(){
-	      var item = $($(this).attr("href"));
-	      if (item.length) { return item; }
-	    });
+  // // Anchors corresponding to menu items
+  // scrollItems = $navItems.map(function(){
+  //   var item = $($(this).attr("href"));
+  //   if (item.length) { return item; }
+  // });
+
+  sticky();
 
   $(window).on('scroll', function () {
 
   	var scrollPos = $(this).scrollTop();
 
   	sticky(scrollPos);
-  	highlightMenuItem(scrollPos);
+  	// highlightMenuItem(scrollPos);
 
   });
 
-
-
-
-  function highlightMenuItem (scrollPos) {
-		// Get id of current scroll item
-		var cur = scrollItems.map(function(){
-		 if ($(this).offset().top < (scrollPos + $navHeight))
-		   return this;
-		});
-
-   	// Get the id of the current element
-   	cur = cur[cur.length-1];
-   	var id = cur && cur.length ? cur[0].id : "";
-   	// Set/remove active class
-   	$navItems.removeClass('active');
-   	$navItems.blur();
-    $navItems.filter('[href=#' + id + ']').addClass('active');
-  }
-
   function sticky (scrollPos) {
-  	if (scrollPos >= navOffset) {
-  		$nav.addClass('stick-sticky-stuck');
-  	} else {
-  		$nav.removeClass('stick-sticky-stuck');
-  	}
+    if (scrollPos >= (aboutOffset - $navHeight)) {
+      $nav.addClass('stick-sticky-stuck');
+    } else {
+      $nav.removeClass('stick-sticky-stuck');
+    }
   }
-
 
   // Resize window
   $(window).on('resize', function () {
-  	navOffset = $navWrapper.offset().top;
+  	aboutOffset = $aboutSection.offset().top;
   	sticky();
   });
-
-
-
 
   // Smooth scroll
 	$('nav a').on('click', function(e) {
@@ -72,6 +49,26 @@ $( function() {
 	    scrollTop: targetOffset - 60
 	  }, 400);
 	});
-} );
+
+
+  // function highlightMenuItem (scrollPos) {
+    // // Get id of current scroll item
+    // var cur = scrollItems.map(function(){
+    //  if ($(this).offset().top < (scrollPos + $navHeight))
+    //    return this;
+    // });
+
+  //    // Get the id of the current element
+  //    cur = cur[cur.length-1];
+  //    var id = cur && cur.length ? cur[0].id : "";
+  //    // Set/remove active class
+  //    $navItems.removeClass('active');
+  //    $navItems.blur();
+  //   $navItems.filter('[href=#' + id + ']').addClass('active');
+  // }
+
+
+
+});
 
 
